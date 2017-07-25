98f2792a0b6c41f93d96a5645ac615f751d92d81
Sahaya Mariadasan <sahaya@gmail.com>
2017-07-25 17:22:08 +0000

    Initials

--- main.rb
+++ main.rb
@@ -2,4 +2,8 @@ module Puzzle
     def self.add(a, b)
         a + b
     end
+    def self.initals(s)
+        s.upcase.split.map(&:chr).join
+    end
+
 end
--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
 require_relative "./main"
 
 p Puzzle.add(8, 10)
+
+p Puzzle.initals("I have a Dog")
